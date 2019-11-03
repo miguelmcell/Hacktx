db034bbe5d2d1b110f29004295cfb90cbfcec21b
alex <robert.perez6@g.austincc.edu>
2019-11-03 00:46:55 -0500

    the get request are done

--- routes.py
+++ routes.py
@@ -1,12 +1,12 @@
 import pymongo
 import json
 from flask import Blueprint
-from requests import get
 from pymongo import MongoClient
+from requests import get
 
 
 client = pymongo.MongoClient("mongodb+srv://alex:b123r456u789@hacktx-wugyn.azure.mongodb.net/test?retryWrites=true&w=majority")
-db = client.ifReturn
+db = client['ifReturn']
 
 rt = Blueprint('rt',__name__)
 
@@ -18,12 +18,37 @@ def index():
 
 @rt.route('/allstates',methods=['GET'])
 def get_states():
-    states=db.collection.find()
+    states=db["prices"]
     responce=[]
-    for states in states:
-        state['_id']=str(state['_id'])
+    for state in states.find():
+        state["_id"]=str(state["_id"])
         responce.append(state)
     return json.dumps(responce)
 
+
+
+@rt.route('/allstate',methods=['GET'])
+def get_state():
+    states=db["prices"]
+    responce=[]
+    for state in states.find({},{"_id":0}):
+        responce.append(state)
+    return json.dumps(responce)
+@rt.route('/juststates',methods=['GET'])
+def just_states():
+    states=db["prices"]
+    responce=[]
+    for state in states.find({},{"_id":0,"House Price":0,"Wealth":0}):
+        responce.append(state)
+    return json.dumps(responce)
+@rt.route("/findstate/<searchstate>",methods=["GET"])
+def find_states(searchstate):
+    responce=[]
+    states=db.prices
+    ans = states.find_one({ "state" : searchstate })
+    #print(ans)
+    #print(type(ans), "____________________")
+    ans = str(ans)
+    return json.dumps(ans)
 
 
